d9abee2007b00dc49944961e4c49dc1235fe71ce
TernenceCao <ternencecao@foxmail.com>
2021-06-12 22:05:20 +0800

    A simple 5-stage-pipeline CPU with forwarding.

---
+++ readme.md
@@ -0,0 +1,2 @@
+# MyCPU
+## A simple 5-stage-pipeline CPU with forwarding.
---
+++ src/testbench.v
@@ -0,0 +1,52 @@
+`timescale 1ns / 1ps
+
+module tb;
+
+    // Inputs
+    reg clk;
+    reg resetn;
+    reg [4:0] rf_addr;
+    reg [31:0] mem_addr;
+
+    // Outputs
+    wire [31:0] rf_data;
+    wire [31:0] mem_data;
+    wire [31:0] IF_pc;
+    wire [31:0] IF_inst;
+    wire [31:0] ID_pc;
+    wire [31:0] EXE_pc;
+    wire [31:0] MEM_pc;
+    wire [31:0] WB_pc;
+    wire [31:0] cpu_5_valid;
+
+    // Instantiate the Unit Under Test (UUT)
+    pipeline_cpu uut (
+        .clk(clk), 
+        .resetn(resetn), 
+        .rf_addr(rf_addr), 
+        .mem_addr(mem_addr), 
+        .rf_data(rf_data), 
+        .mem_data(mem_data), 
+        .IF_pc(IF_pc), 
+        .IF_inst(IF_inst), 
+        .ID_pc(ID_pc), 
+        .EXE_pc(EXE_pc), 
+        .MEM_pc(MEM_pc), 
+        .WB_pc(WB_pc), 
+        .cpu_5_valid(cpu_5_valid)
+    );
+
+    initial begin
+        // Initialize Inputs
+        clk = 0;
+        resetn = 0;
+        rf_addr = 0;
+        mem_addr = 0;
+
+        // Wait 100 ns for global reset to finish
+        #100;
+      resetn = 1;
+        // Add stimulus here
+    end
+   always #5 clk=~clk;
+endmodule
